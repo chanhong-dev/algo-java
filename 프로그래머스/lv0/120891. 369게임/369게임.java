b4c0c9e24e2fd921ea9966d57a79cca63734b355
chanhong-dev <90807343+chanhong-dev@users.noreply.github.com>
2023-02-04 22:58:57 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        int temp = 0;
+        
+        while(order > 0){
+            temp = order % 10;
+            
+            if(temp == 3 || temp == 6 || temp == 9){
+                answer++;
+            }
+            
+            order /= 10;
+            
+        }
+        return answer;
+    }
+}
